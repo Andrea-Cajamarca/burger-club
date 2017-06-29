95f5c4b639debb8c9eee61ca00ae86f054b83297
andrea_cajamarca <andreakcajamarca@gmail.com>
2017-06-29 20:19:16 +0000

    social media mod and scripted location implemented

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
+/*global $ */
 $(document).ready(function() {
-    $('#myModal').modal('toggle');
-    $('#myModal').modal('show');
-    $('#myModal').modal('hide');
-
-    // $("#results").text(data);
-
-    ev.preventDefault();
+    var email = $("input:text").val();
+    var name = $("input:text").val();
+    $("#myModal").click(function() {
+        //work on this later
+         alert("Hi " + name + ". Thank you for signing up! We will send an email to"  + email + " shortly!");    
+    });
 });
-    
-*/
 
-});
+$(".social_img").hover(function() {
+        $(this).animate({
+            paddingTop: "15px"
+        }, 100);
+    }, function() {
+        $(this).animate({
+            paddingTop: "10px"
+        }, 100);
+        
+    });
